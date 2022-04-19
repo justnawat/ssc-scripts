f225741a2f3a7d17aa0485db36ecc9627f6dbeeb
Nawat Ngerncham <n.ngerncham@gmail.com>
2022-04-20 00:27:21 +0700

    combine_member ignore cmts and strip()s

--- combine_members.py
+++ combine_members.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import glob
 
-path = "D:\\sciso-club\\documents\\trim-report"
+# identifying all response files
+path = "D:\\sciso-club\\documents\\trim-report" # to be changed
 filenames = glob.glob(path + "\*.csv")
 
+# loads the data from response csv
 big_df = pd.DataFrame()
 col_names = ["Full Name (First Name & Surname)", "Student ID"]
 for name in filenames:
 	df = pd.read_csv(name, usecols=col_names)
 	big_df = pd.concat([big_df, df], ignore_index=True)
 
+# for checking if member has shown up in past activities or not
 name_set = set()
 name_id = []
 def cond(data):
@@ -20,7 +23,7 @@ def cond(data):
 	conds.append(name not in name_set)
 	return len(conds) != 0 and all(conds)
 
-# big_df.to_csv("tmp.csv", index=False)
+# making a list of all names
 for _, data in big_df.iterrows():
 	name = data.get("Full Name (First Name & Surname)")
 	stuid = data.get("Student ID")
@@ -29,9 +32,33 @@ for _, data in big_df.iterrows():
 		name_set.add(name)
 		name_id.append((name, int(stuid)))
 
+# sort names and IDs
 sorted_nid = sorted(name_id, key=lambda o: o[1])
-names = [ o[0] for o in sorted_nid ]
-ids = [ o[1] for o in sorted_nid ]
+
+# committee names to be skipped, check every term
+# TODO: make it read from a file
+cmts_names = [ 
+	"Tanisha Abrol",
+	"Shormeli Akter",
+	"Nawat Ngerncham",
+	"Alexander May",
+	"Irin Kortonglang",
+	"Ratvarin Teankrajang",
+	"Supapitch Ngamsangapong",
+	"Thitika Wangsanutr ",
+	"Justin Copeland",
+	"Patcharanan Janpakasawat",
+	"Nichaphatr Suvajanakorn",
+	"Piyakorn Rodthanong",
+]
+
+# writing to a csv file
+names = []
+ids = []
+for i, (name, sid) in enumerate(sorted_nid):
+	if name not in cmts_names:
+		names.append(name.strip())
+		ids.append(sid)
 df = pd.DataFrame({
 	"names": names,
 	"ids": ids
