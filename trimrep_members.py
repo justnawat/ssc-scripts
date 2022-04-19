da5ce4b97977481d28ad57597c710e791b9189a2
Nawat Ngerncham <n.ngerncham@gmail.com>
2022-04-20 00:55:35 +0700

    trimrep_members for add member to trimester report

---
+++ trimrep_members.py
@@ -0,0 +1,26 @@
+import pyautogui as pg
+import pandas as pd
+from time import sleep
+pg.FAILSAFE = True
+
+path = "C:\\Users\\nnger\\sandbox\\out.csv"
+df = pd.read_csv(path)
+
+i = 12
+sleep(5)
+for _, data in df.iterrows():
+	name = data.get("names")
+	sid = data.get("ids")
+	i += 1
+
+	# adding number
+	pg.press("tab")
+	pg.write(str(i) + ".")
+	pg.press("tab")
+
+	# name, id, pos
+	pg.write(name)
+	pg.press("tab")
+	pg.write(str(sid))
+	pg.press("tab")
+	pg.write("Member")
